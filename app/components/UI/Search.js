ca1a7e2db0b5d9a8dfcb61b195df3d1c17c8d8db
Radim Hejduk <dev@radimhejduk.cz>
2024-03-20 10:44:38 +0100

    search working

--- app/components/UI/Search.js
+++ app/components/UI/Search.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { document } from "postcss";
 import { useState } from "react";
 
 const Search = () => {
@@ -23,6 +24,11 @@ const Search = () => {
             .then(json => {
                 setResults(json.results);
             })
+    };
+
+    const resetSearchHandler = () => {
+        setQuery(null);
+        setResults([]);
     };
 
     const textPart = (item) => {
@@ -50,12 +56,12 @@ const Search = () => {
                 alt="hledat"
                 className="search-glass"
             />
-            <input type="text" className="w-1/2 search-bar" onChange={searchHandler} />
-            <div id="search-results" className={results.length > 0 ? "block" : "hidden"}>
+            <input type="text" id="search-bar" className="w-1/2 search-bar" onChange={searchHandler} />
+            <div id="search-results" className={results.length > 0 ? "absolute bg-gray-100 left-1/2 translate-x-[-50%] mt-1" : "hidden"}>
                 {Object.values(results).map((item) => {
                     return (
-                        <div key={item.page}>
-                            <Link href={item.page}>{item.title}</Link><br />
+                        <div key={item.page} className="mt-1 border border-gray-300 rounded p-4">
+                            <Link href={item.page} onClick={resetSearchHandler}>{item.title}</Link><br />
                             <p className="text-sm text-gray-700">{textPart(item)}</p>
                         </div>
                     );
