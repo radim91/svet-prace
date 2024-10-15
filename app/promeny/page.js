f5b890b35956a6bc3ae7cd0bc6185f303be546ae
Radim Hejduk <dev@radimhejduk.cz>
2024-10-15 14:28:45 +0200

    WIP changes

--- app/promeny/page.js
+++ app/promeny/page.js
@@ -2,16 +2,12 @@
 
 import ImageBox from "@/components/UI/ImageBox";
 import Image from "next/image";
-import { useState } from "react";
-/* import HoverBox from "../../components/UI/HoverBox"; */
-/* import HoverLink from "../../components/UI/HoverLink"; */
-/* import ImageBox from "../../components/UI/ImageBox"; */
-/* import InfoBox from "../../components/UI/InfoBox"; */
-/* import Quote from "../../components/UI/Quote"; */
-/* import Link from "next/link"; */
+import { ToggleModeContext } from "../../context/ToggleModeContext";
+import { useContext } from "react";
+import Mode from "@/enum/mode";
 
 const ChangesPage = () => {
-    const [hoverShown, setHoverShown] = useState(null);
+    const { toggleMode } = useContext(ToggleModeContext);
 
     return (
         <>
@@ -37,13 +33,37 @@ const ChangesPage = () => {
                     Lidé se rodili, žili a umírali na stejném místě – nebo nepříliš
                     daleko od něj.
                 </p>
-                <div className="mt-4">
+                <div className="mt-4 mb-4">
                     <ImageBox
                         foreground="/images/chapters/changes/01.webp"
                         background="/images/chapters/changes/01-line.svg"
                         additionalClass="absolute mt-24 z-100"
                     />
                 </div>
+                <p className="mb-4">
+                    Základem bylo zemědělství – všechno se pěstovalo&nbsp;
+                    <span className={`${toggleMode === Mode.FUN ? "green" : ""}`}>lokálně</span> pro
+                    potřeby místní komunity.
+                </p>
+                <p className={`transition-all duration-500 ease-in-ou overflow-hidden ${toggleMode === Mode.FUN ? "max-h-96 opacity-100 green mb-4" : "max-h-0 opacity-0"}`}>
+                    Za podobná privilegia si my dnes musíme připlatit. Ale zase se můžeme podívat,
+                    kam chceme (když na to budeme mít).
+                </p>
+                <p className="mb-4">
+                    Hrstka bohatých, urozených a mocných půdu dědičně vlastnila, ostatní populace na ní
+                    “dědičně” pracovala. Semtam se někdo vzepřel pravidlům, podle kterých se “odjakživa”
+                    žilo, ale pohyb mezi různými vrstvami společnosti nebyl příliš častý.
+                </p>
+                <p className="mb-4">
+                    Proč to tak bylo? Má to spoustu důvodů. Feudalismus, například. Tyto drobné životy
+                    žili lidé v rámci obřích říší, kterým vládli panovníci, kteří vládli dalším panovníkům
+                    a tak dále – a úplně nejníž byli skoro všichni ostatní. Bylo jich nejvíc, ale měli nejmenší moc.
+                </p>
+                <p className="mb-4">
+                    Nebo taky náboženství a myšlenkové prostředí – ať už za tím byl Bůh či tradice komunity,
+                    vesměs se mělo za to, že život je tak nějak daný, cyklicky se opakující proces a v něm
+                    má každý svůj daný osud.
+                </p>
             </div>
         </>
     );
