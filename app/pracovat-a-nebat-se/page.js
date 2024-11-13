0fbf6f40f9fd5dbc57660b41c4980730000a568a
Radim Hejduk <dev@radimhejduk.cz>
2024-11-13 20:26:09 +0100

    WIP chapter two

--- app/pracovat-a-nebat-se/page.js
+++ app/pracovat-a-nebat-se/page.js
@@ -279,11 +279,203 @@ const FearlessPage = () => {
                     <div className="blue mb-8">
                         zde bude anketa
                     </div>
-                    <p className="mb-4">
+                    <p className="mb-8">
                         Po legalizaci postupně odborové organizace vypracovaly a systematizovaly různé nástroje na zlepšení podmínek práce: na jedné straně se
                         stále více prosazovalo kolektivní vyjednávání o podobě kolektivní smlouvy, kdy zástupci dělníků
-                        <span className="green">jednají s vedením podniku</span> o svých pracovních poměrech.
+                        {" "}<span className="green">jednají s vedením podniku</span> o svých pracovních poměrech.
                     </p>
+                    <div className={`transition-all duration-500 ease-in-out overflow-hidden green ${toggleMode === Mode.FUN ? "opacity-100" : "max-h-0 opacity-0"}`}>
+                        <Image
+                            src="/images/chapters/fearless/12.jpg"
+                            width={400}
+                            height={400}
+                            alt="obr 12"
+                            className="mb-8 rounded mx-auto"
+                        />
+                        <p className="mb-4">
+                            Takové smírné jednání je v zájmu obou stran: zaměstnavatel se sice vzdá části svých zisků a například zvýší mzdy, výměnou za to ale
+                            získává spokojenost a loajalitu zaměstnanců, kteří budou pracovat líp a zůstanou u něj delší dobu, takže v součtu zvýší i on svůj
+                            zisk. Dnešní řečí by se tomu dalo říct “win-win situation”.
+                        </p>
+                    </div>
+                    <p className="mb-4">
+                        V případě, kdy nebylo dosaženo dohody, odbory rozvíjely protestní činnost, která mohla vyústit i 
+                        do <span className="green">stávky</span>. Legalizace stávky se také běžně
+                        stávala součástí zákonů legalizující odborovou činnost.
+                    </p>
+                    <p className={`transition-all duration-500 ease-in-out overflow-hidden green ${toggleMode === Mode.FUN ? "opacity-100" : "max-h-0 opacity-0"}`}>
+                        Stávka, tedy situace, kdy zaměstnanci prostě “nejdou do práce”, je u nás ještě stále vnímána často negativně. Její účastníci jsou
+                        “nemakačenka”, kteří jen chamtivě chtějí víc. Ve skutečnosti jde o geniální strategii, jak bez násilí bojovat s někým mnohem silnějším.
+                    </p>
+                    <Image
+                        src="/images/chapters/fearless/13.jpg"
+                        width={600}
+                        height={400}
+                        alt="obr 7"
+                        className="mb-8 rounded mx-auto"
+                    />
+                    <p className="mb-4 text-center">
+                        Stávkovat není žádná ostuda – dělají to i milionáři.
+                    </p>
+                    <p className="mb-4">
+                        V prvních dekádách dvacátého století dosáhly odbory mnoha dříve nepředstavitelných vymožeností. Dětství bez práce, starobní důchody,
+                        bezpečnost a důstojnost práce, nárok na dovolenou či zákonnou garanci minimální mzdy a maximální pracovní doby se pomalu stávaly realitou.
+                        Ty nejhorší nešvary z doby formování kapitalismu se podařilo se zlepšením podmínek práce a životů pracujících výrazně zkrotit.
+                    </p>
+                    <p className="mb-8">
+                        Původně vykořenění a strádající dělníci a dělnice na přelomu 19. a 20. století vstupovali do spolků, sportovních klubů, v tisku se
+                        vyjadřovali k veřejným záležitostem nebo mohli volit své zástupce do parlamentu.
+                    </p>
+                    <Image
+                        src="/images/chapters/fearless/14.jpg"
+                        width={400}
+                        height={400}
+                        alt="obr 14"
+                        className="mb-12 rounded mx-auto"
+                    />
+                </div>
+                <div className="mt-4 mb-4 relative">
+                    <CollapseButton shown={shown} setShown={shownHandler} id="2" />
+                    <h3 className="text-center text-2xl blue mb-4">
+                        <span className="me-8">2.2</span>Give peace a chance
+                        <br/>aneb Odbory jako strůjce sociálního smíru (1918–1939)
+                    </h3>
+                </div>
+                <div className={`mb-12 ${shown.includes('2') ? "block" : "hidden"}`}>
+                    <div className="absolute right-0 -mt-24">
+                        <Image
+                            src="/images/chapters/fearless/paper.svg"
+                            width={200}
+                            height={200}
+                            alt="Papír"
+                        />
+                    </div>
+                    <p className="mt-8 mb-4">
+                        První světová válka otřásla základy společnosti, jejího přemýšlení i fungování. Kromě <span className="green">všeho strašného</span>{" "}
+                        ale přinesla i vlnu optimismu a naladění ke změnám.
+                    </p>
+                    <div className={`transition-all duration-500 ease-in-out overflow-hidden green ${toggleMode === Mode.FUN ? "opacity-100" : "max-h-0 opacity-0"}`}>
+                        <iframe 
+                            width="560"
+                            height="315"
+                            src="https://www.youtube.com/embed/qFqgmaO15x4?si=xJpnSvnbBPJRYFgn"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerpolicy="strict-origin-when-cross-origin"
+                            allowfullscreen
+                            className="mt-4 mb-8 rounded mx-auto"
+                        ></iframe>
+                        <p className="mb-4">
+                            S podobně rozsáhlou katastrofou se lidstvo do té doby nepotýkalo. První světová válka přinesla ponorky, tanky, automatické zbraně jako
+                            kulomety a dalekonosná děla, chemické zbraně, smrtící plyny, nekonečně utrpení v zákopech,rozšíření chorob jako byla pandemická španělská
+                            chřipka... Zemřelo v ní asi 10 milionů vojáků a další miliony civilistů, ovlivnila porodnost, přinesla stovky tisíc sirotků a mrzáků,
+                            rozvrátila hospodářskou stabilitu, způsobila miliony traumat a ukázala lidskou schopnost sebezničení v úplně novém, globálním světle.
+                        </p>
+                    </div>
+                    <Image
+                        src="/images/chapters/fearless/15.png"
+                        width={1200}
+                        height={400}
+                        alt="obr 15"
+                        className="mb-8 rounded mx-auto"
+                    />
+                    <p className="mb-4">
+                        Konec první světové války přinesl taky surrealismus, dadaismus, průmyslově vyráběné vložky nebo rozšíření jazzu.
+                    </p>
+                    <p className="mb-4">
+                        S pádem stoletých impérií přišel i nový demokratický optimismus. Svět po roce 1918 měl být zcela jiný než svět
+                        před rokem 1914. Důležitým tématem se staly potřeby obyčejných lidí, což s sebou přineslo i změnu přístupu k odborovým organizacím.
+                    </p>
+                    <p className="mb-4">
+                        Stále více států uznávalo, že sociální rovnost a spravedlnost na pracovišti jsou důležitými součástmi fungující demokratické společnosti.
+                        Možná, že spokojenost dělníků a obyčejných lidí zabrání další podobné katastrofě! A tak zatímco dříve byly odbory brány jako nátlakový
+                        nástroj a zdroj konfliktů, teď se věřilo, že můžou sloužit míru.
+                    </p>
+                    <p className="mb-8">
+                        Meziválečné období je tak možno označit jako počátek moderního sociálního dialogu.
+                    </p>
+                    <Image
+                        src="/images/chapters/fearless/16.jpg"
+                        width={600}
+                        height={400}
+                        alt="obr 16"
+                        className="mb-8 rounded mx-auto"
+                    />
+                    <p className="mb-4">
+                        Všechno ale nebylo růžové.
+                    </p>
+                    <p className="mb-4">
+                        Míra pracovních práv se stala jedním z důležitých ukazatelů vyspělosti politické kultury. Ta ale ne všude směřovala k demokracii. 
+                    </p>
+                    <p className="mb-4">
+                        Není náhodou, že jeden z prvních kroků fašistické Itálie, nacistického Německa, frankistického Španělska i komunistických režimů
+                        v Sovětském svazu a později ve východní Evropě byl zákaz svobodné odborové činnosti. Podle nacistů a fašistů neexistovalo nic jako
+                        specifické zájmy dělnictva, ale pouze zájmy národa. Komunistické režimy pak o sobě tvrdily, že jejich nejvyšší zájem je zlepšení
+                        situace pracujících a existence svobodných odborů prý už neměla smysl. Ve skutečnosti ale všechny tyto režimy chtěly
+                        {" "}<span className="green">vše podřídit státní kontrole</span>.
+                    </p>
+                    <div className={`transition-all duration-500 ease-in-out overflow-hidden green ${toggleMode === Mode.FUN ? "opacity-100" : "max-h-0 opacity-0"}`}>
+                        <Image
+                            src="/images/chapters/fearless/17.svg"
+                            width={600}
+                            height={400}
+                            alt="obr 17"
+                            className="mt-2 mb-6 rounded mx-auto"
+                        />
+                        <p className="mb-4">
+                            Totalitaristické ideologie se nestavěly do opozice k zájmu pracujících v tom smyslu, že by se chtěly vrátit o sto let nazpět.
+                            Naopak, určité jistoty, bezpečí nebo pohodlí svým stoupencům nabízely, ale rozhodně s nimi nevedly dialog – spíše monolog.
+                            Navíc šlo vždy o zajištění bezpečí “jen pro ty naše”. Žádná univerzální práva a spojenectví. Naopak, totalitaristické ideologie
+                            dojily chaos poválečné doby budováním imaginárních nepřátel, kteří mohou za všechno. Až po jejich poražení bude možné dosáhnout
+                            spravedlivé a spokojené společnosti.
+                        </p>
+                    </div>
+                    <p className="mb-4">
+                        I proto byla mírová a spojující role odborových organizací stále více rozeznávána. Není náhodou, že první Mezinárodní organizace
+                        práce (MOP), jež byla založená roku 1919, vznikla pod záštitou <span className="green">Společnosti národů</span>.
+                    </p>
+                    <div className={`transition-all duration-500 ease-in-out overflow-hidden green ${toggleMode === Mode.FUN ? "opacity-100" : "max-h-0 opacity-0"}`}>
+                        <p className="mb-8">
+                            Společnost národů (SN), předchůdce dnešní OSN, vznikla jako přímá reakce na hrůzy první světové války s cílem udržet celosvětový
+                            mír. Kromě úkolů jako řešit mezinárodní konflikty nebo omezit zbrojení měla na starost i lidskoprávní a sociální otázky. Se svým
+                            mírovým cílem SN selhala – jak víme, došlo i na druhou světovou válku. Agendu lidských a sociálních práv se ale dařilo zapisovat
+                            hlouběji a hlouběji do kolektivního vědomí.
+                        </p>
+                        <Image
+                            src="/images/chapters/fearless/18.jpg"
+                            width={400}
+                            height={400}
+                            alt="obr 18"
+                            className="mb-8 rounded mx-auto"
+                        />
+                    </div>
+                    <p className="mb-4">
+                        MOP v období mezi světovými válkami dosáhla značných úspěchů: prosadila osmihodinovou pracovní dobu do legislativ mnoha evropských států
+                        i další sociální zákony. Postupně se stala respektovanou mezinárodní institucí, jež garantuje svobodu odborového sdružování, definuje
+                        minimální míru sociální spravedlnosti uvnitř států, které se chtějí nazývat demokratické, nebo pravidelně iniciuje jednání
+                        zástupců vlád, zaměstnavatelů a zaměstnanců.
+                    </p>
+                    <Image
+                        src="/images/chapters/fearless/19.png"
+                        width={400}
+                        height={400}
+                        alt="obr 19"
+                        className="mb-8 rounded mx-auto"
+                    />
+                    <p className="mb-4">
+                        V roce 1969 získala MOP u příležitosti své 50leté činnosti dokonce Nobelovu cenu míru.
+                    </p>
+                    <p className="mb-8">
+                        Když si to shrneme – jaké byly výsledky sociálního dialogu do konce druhé světové války?
+                    </p>
+                    <Image
+                        src="/images/chapters/fearless/20.svg"
+                        width={600}
+                        height={400}
+                        alt="obr 20"
+                        className="mb-8 rounded mx-auto"
+                    />
                 </div>
             </div>
         </>
